b9d3954db6ca8d2429a10cb9f226d140c8976be3
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-21 04:20:31 +0530

    feat: acceptLoop() and err handling

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Server struct {
 	listenAddr string
@@ -11,19 +14,31 @@ type Server struct {
 func newServer(listenAddr string) *Server {
 	return &Server{
 		listenAddr: listenAddr,
+		quitCh: make(chan struct{}),
 	}
 }
 
 func (s *Server) start() error {
 	ln, err := net.Listen("tcp", s.listenAddr)
-
 	if err != nil {
 		return err
 	}
-
 	defer ln.Close()
-
 	s.ln = ln
+
+	<- s.quitCh
+
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("Accepting ERR:", err)
+			continue
+		}
+	}
 }
 
 func main() {
