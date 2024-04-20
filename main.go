ab6cb5a2d72b16e3be257b662b9f2e69c32eb213
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-21 04:17:38 +0530

    feat: start()

--- main.go
+++ main.go
@@ -5,12 +5,25 @@ import "net"
 type Server struct {
 	listenAddr string
 	ln net.Listener
+	quitCh chan struct{}
 }
 
 func newServer(listenAddr string) *Server {
 	return &Server{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *Server) start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+
+	s.ln = ln
 }
 
 func main() {
